83af8e35c7ead199035b6f8fadd3f9eae736a66e
hachi <eojin312@naver.com>
2020-07-25 22:42:27 +0900

    MediaType 을 json 타입으로 할 수 있게 선언해주기 + 테스트까지 완료

--- src/test/java/hachi/web/WhiteShipSampleControllerTest.java
+++ src/test/java/hachi/web/WhiteShipSampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,7 +30,8 @@ class WhiteShipSampleControllerTest {
 
     @Test
     void 요청테스트() throws Exception {
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello")
+                .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"))
