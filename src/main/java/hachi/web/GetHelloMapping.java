53c44f3c9db6860b385e1a53156d6541418386f9
hachi <eojin312@naver.com>
2020-07-30 16:03:03 +0900

    커스텀 어노테이션 target과 documented 추가

--- src/main/java/hachi/web/GetHelloMapping.java
+++ src/main/java/hachi/web/GetHelloMapping.java
@@ -3,6 +3,11 @@ package hachi.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD) // GetHelloMapping 어노테이션이 어디에 사용될 수 있는지 (METHOD) 만 사용가
+@Retention(RetentionPolicy.RUNTIME) // 해당 어노테이션 정보가 언제까지 유지할 것인가
 @RequestMapping(method = RequestMethod.GET, value = "/hello")
 public @interface GetHelloMapping {
 }
